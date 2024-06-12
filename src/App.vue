61abd42f6db4cbde8335432970363c4e62289a79
leon <leon.liu@accuhit.net>
2024-06-12 16:11:38 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <main>
+    <p>
+      Test page
+    </p>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+main{
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
